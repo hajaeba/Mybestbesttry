21e59f9d2d58518a33811ae81f222c99fe0b83a0
haju <hajaebakemal@gmail.com>
2022-07-29 12:53:09 -0500

    This is my try

---
+++ C.java
@@ -0,0 +1,11 @@
+package pack2;
+import pack1.A;
+public class C extends A{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  C obj = new C();
+ obj.methodOne();
+	}
+
+}
